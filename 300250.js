5070c9ee6badc0d56bc3ed06dcb5b4ee8d57fb23
zgomotos <gogu.samsung@gmail.com>
2021-02-22 13:12:49 +0200

    Create 300250.js

---
+++ 300250.js
@@ -0,0 +1,9 @@
+ function prepareFrame() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://maller.ro/ads/300250.html");
+        ifrm.style.width = "300px";
+        ifrm.style.height = "250px";
+        document.body.appendChild(ifrm);
+    }
+    
+    prepareFrame()
