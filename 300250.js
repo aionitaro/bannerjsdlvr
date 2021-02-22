f8021ae965892fece3d6f793525e664501f6b8f2
zgomotos <gogu.samsung@gmail.com>
2021-02-22 13:18:16 +0200

    Update 300250.js

--- 300250.js
+++ 300250.js
@@ -3,6 +3,7 @@
         ifrm.setAttribute("src", "https://maller.ro/ads/300250.html");
         ifrm.style.width = "300px";
         ifrm.style.height = "250px";
+        ifrm.scrolling = "no";
         document.body.appendChild(ifrm);
     }
     
